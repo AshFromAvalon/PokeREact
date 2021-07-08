0add25b7d944e090f7f4a3d07c6198ffa4594a3f
Thomas Le Bihan <thomas.66days@gmail.com>
2021-07-08 17:10:10 +0200

    filter selection OK

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,30 +3,35 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import NavBar from "../NavBar";
 import PokeRow from "../PokeRow/index";
+import colors from "../../utils/typesColors";
 
 const TYPES = [
-  "grass",
-  "poison",
-  "fire",
-  "flying",
-  "water",
-  "bug",
-  "normal",
-  "electric",
-  "ground",
-  "fairy",
-  "fighting",
-  "psychic",
-  "rock",
-  "steel",
-  "ice",
-  "ghost",
-  "dragon",
+  { name: "grass", isSelected: false },
+  { name: "poison", isSelected: false },
+  { name: "fire", isSelected: false },
+  { name: "flying", isSelected: false },
+  { name: "water", isSelected: false },
+  { name: "bug", isSelected: false },
+  { name: "normal", isSelected: false },
+  { name: "electric", isSelected: false },
+  { name: "ground", isSelected: false },
+  { name: "fairy", isSelected: false },
+  { name: "fighting", isSelected: false },
+  { name: "psychic", isSelected: false },
+  { name: "rock", isSelected: false },
+  { name: "steel", isSelected: false },
+  { name: "ice", isSelected: false },
+  { name: "ghost", isSelected: false },
+  { name: "dragon", isSelected: false },
 ];
 
 function App() {
   const [pokemons, setPokemons] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [types, setTypes] = useState(TYPES);
+  const [selectedType, setSelectedType] = useState(null);
+
+  const colorMap = colors;
 
   useEffect(() => {
     const fecthData = async () => {
@@ -41,6 +46,34 @@ function App() {
     fecthData();
   }, []);
 
+  const handleSelect = (selection) => {
+    const typesCopy = [...types];
+    const selectedType = typesCopy.find((type) => type.name === selection);
+    const currentSelectedType = typesCopy.find(
+      (type) => type.isSelected === true
+    );
+
+    let typeSelection = null;
+
+    if (currentSelectedType && currentSelectedType === selectedType) {
+      selectedType.isSelected = false;
+    }
+
+    if (currentSelectedType && currentSelectedType != selectedType) {
+      currentSelectedType.isSelected = false;
+      selectedType.isSelected = true;
+      typeSelection = selection;
+    }
+
+    if (!currentSelectedType) {
+      typeSelection = selection;
+      selectedType.isSelected = true;
+    }
+
+    setSelectedType(typeSelection);
+    setTypes(typesCopy);
+  };
+
   if (isLoading) {
     return (
       <main className="main">
@@ -50,19 +83,37 @@ function App() {
     );
   }
 
-  // !isLoading && console.log(pokemons.results[0]);
-  //poke.details.sprites.other["official-artwork"].front_default
-
   return (
     <main className="main">
       <header>POKEMON</header>
       <div className="main-container">
-        <NavBar types={TYPES} />
+        <NavBar>
+          {types.map((type) => {
+            return (
+              <button
+                onClick={() => handleSelect(type.name)}
+                className={type.isSelected ? "btn btn-active" : "btn"}
+                key={type.name}
+                style={
+                  type.isSelected
+                    ? { color: "white" }
+                    : { color: colorMap[type.name] || colorMap.default }
+                }
+              >
+                {type.name}
+              </button>
+            );
+          })}
+        </NavBar>
         <ul className="list">
           {pokemons.results.map((poke, index) => {
             return (
               <li key={poke.url}>
-                <PokeRow pokemon={poke} index={index} />
+                <PokeRow
+                  pokemon={poke}
+                  index={index}
+                  selectedType={selectedType}
+                />
               </li>
             );
           })}
--- src/components/PokeRow/index.js
+++ src/components/PokeRow/index.js
@@ -2,8 +2,9 @@ import "./style.pokerow.scss";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const PokeRow = ({ pokemon, index }) => {
+const PokeRow = ({ pokemon, index, selectedType }) => {
   const [infos, setInfos] = useState();
+  const [types, setTypes] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -12,7 +13,8 @@ const PokeRow = ({ pokemon, index }) => {
       const details = await axios.get(response.data.species.url);
 
       response.data.details = details.data;
-
+      const types = response.data.types.map((poke) => poke.type.name);
+      setTypes(types);
       setInfos(response.data);
       setIsLoading(false);
     };
@@ -27,30 +29,31 @@ const PokeRow = ({ pokemon, index }) => {
     );
   }
 
-  !isLoading && console.log(infos);
-
   const rowBackgroundColorStyle = index % 2 === 0 ? "#F8F6F7" : "#FDFDFD";
+  const showLine = selectedType ? types.includes(selectedType) : true;
 
   return (
-    <div
-      className="row split"
-      style={{ backgroundColor: rowBackgroundColorStyle }}
-    >
-      <div className="card">
-        <img
-          className="card-image"
-          src={infos.sprites.other["official-artwork"].front_default}
-          alt=""
-        />
+    showLine && (
+      <div
+        className="row split"
+        style={{ backgroundColor: rowBackgroundColorStyle }}
+      >
+        <div className="card">
+          <img
+            className="card-image"
+            src={infos.sprites.other["official-artwork"].front_default}
+            alt=""
+          />
+        </div>
+        <div className="card">
+          <img
+            className="card-image"
+            src={infos.sprites.other["official-artwork"].front_default}
+            alt=""
+          />
+        </div>
       </div>
-      <div className="card">
-        <img
-          className="card-image"
-          src={infos.sprites.other["official-artwork"].front_default}
-          alt=""
-        />
-      </div>
-    </div>
+    )
   );
 };
 
